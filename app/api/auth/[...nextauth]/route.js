59c65c48385e536e31c630c7991583a492f330a8
Rajiv S <rajiv620019@gmail.com>
2023-07-18 12:58:10 +0530

    Created signIn functions for new & existing users

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import { User } from "@models/user";
 
 // Handler to handle the authentication
 const handler = NextAuth({
@@ -15,8 +16,25 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       connectToDB();
+
+      // Check is a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      // If not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
     } catch (error) {
       console.log(error);
+      return false;
     }
   },
 });
