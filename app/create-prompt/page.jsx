51fe5bcc87e123c05db3708138a02bade5482b27
Rajiv S <rajiv620019@gmail.com>
2023-07-19 18:30:05 +0530

    Made some changes

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,21 +7,45 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
+  const router = useRouter();
+  const { data: session } = useSession();
 
-  const createPrompt = async () => {};
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
-  return;
-  <Form
-    type="Create"
-    post={post}
-    setPost={setPost}
-    submitting={submitting}
-    handleSubmit={createPrompt}
-  />;
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
+
 export default CreatePrompt;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Form = (type, post, setPost, submitting, handleSubmit) => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
@@ -8,58 +8,46 @@ const Form = (type, post, setPost, submitting, handleSubmit) => {
       </h1>
       <p className="desc text-left max-w-md">
         {type} and share amazing prompts with the world, and let your
-        imagination run wild with any AI-powered platform.
+        imagination run wild with any AI-powered platform
       </p>
 
       <form
         onSubmit={handleSubmit}
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
       >
-        {/* Label for AI prompt */}
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
-            Your AI prompt
+            Your AI Prompt
           </span>
 
           <textarea
             value={post.prompt}
-            onChange={(e) =>
-              setPost({
-                ...post,
-                prompt: e.target.value,
-              })
-            }
-            placeholder="Write your prompt here"
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your post here"
             required
-            className="form_textarea"
+            className="form_textarea "
           />
         </label>
 
-        {/* Label for Tags */}
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
-            Tag {` `}
+            Field of Prompt{" "}
             <span className="font-normal">
-              (#product, #webdevelopment, #idea)
+              (#product, #webdevelopment, #idea, etc.)
             </span>
           </span>
-
           <input
             value={post.tag}
-            onChange={(e) =>
-              setPost({
-                ...post,
-                tag: e.target.value,
-              })
-            }
-            placeholder="#tag"
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            type="text"
+            placeholder="#Tag"
             required
             className="form_input"
           />
         </label>
 
         <div className="flex-end mx-3 mb-5 gap-4">
-          <Link href="/" className="text-gray-500">
+          <Link href="/" className="text-gray-500 text-sm">
             Cancel
           </Link>
 
@@ -68,11 +56,12 @@ const Form = (type, post, setPost, submitting, handleSubmit) => {
             disabled={submitting}
             className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
           >
-            {submitting ? `${type}...` : type}
+            {submitting ? `${type}ing...` : type}
           </button>
         </div>
       </form>
     </section>
   );
 };
+
 export default Form;
